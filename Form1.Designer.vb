75b8712fc6a4261ff7db09f4ffd2e75771090e8c
kindaw <ratchavit3@gmail.com>
2024-08-05 11:12:01 +0700

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckBox_Showpassword = New System.Windows.Forms.CheckBox()
+        Me.btb_Login = New System.Windows.Forms.Button()
+        Me.lbl_Usernam = New System.Windows.Forms.Label()
+        Me.lbl_Password = New System.Windows.Forms.Label()
+        Me.txt_Username = New System.Windows.Forms.TextBox()
+        Me.txt_Password = New System.Windows.Forms.TextBox()
+        Me.lbl_Login = New System.Windows.Forms.Label()
+        Me.lbl_WelcomeBack = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'CheckBox_Showpassword
+        '
+        Me.CheckBox_Showpassword.AutoSize = True
+        Me.CheckBox_Showpassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox_Showpassword.Location = New System.Drawing.Point(85, 259)
+        Me.CheckBox_Showpassword.Name = "CheckBox_Showpassword"
+        Me.CheckBox_Showpassword.Size = New System.Drawing.Size(136, 22)
+        Me.CheckBox_Showpassword.TabIndex = 0
+        Me.CheckBox_Showpassword.Text = "Show Password"
+        Me.CheckBox_Showpassword.UseVisualStyleBackColor = True
+        '
+        'btb_Login
+        '
+        Me.btb_Login.BackColor = System.Drawing.Color.BlueViolet
+        Me.btb_Login.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btb_Login.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.btb_Login.Location = New System.Drawing.Point(85, 287)
+        Me.btb_Login.Name = "btb_Login"
+        Me.btb_Login.Size = New System.Drawing.Size(410, 53)
+        Me.btb_Login.TabIndex = 1
+        Me.btb_Login.Text = "Login"
+        Me.btb_Login.UseVisualStyleBackColor = False
+        '
+        'lbl_Usernam
+        '
+        Me.lbl_Usernam.AutoSize = True
+        Me.lbl_Usernam.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Usernam.Location = New System.Drawing.Point(79, 170)
+        Me.lbl_Usernam.Name = "lbl_Usernam"
+        Me.lbl_Usernam.Size = New System.Drawing.Size(139, 31)
+        Me.lbl_Usernam.TabIndex = 2
+        Me.lbl_Usernam.Text = "Username"
+        '
+        'lbl_Password
+        '
+        Me.lbl_Password.AutoSize = True
+        Me.lbl_Password.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Password.Location = New System.Drawing.Point(79, 218)
+        Me.lbl_Password.Name = "lbl_Password"
+        Me.lbl_Password.Size = New System.Drawing.Size(134, 31)
+        Me.lbl_Password.TabIndex = 3
+        Me.lbl_Password.Text = "Password"
+        '
+        'txt_Username
+        '
+        Me.txt_Username.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Username.ForeColor = System.Drawing.Color.Gray
+        Me.txt_Username.Location = New System.Drawing.Point(216, 169)
+        Me.txt_Username.Name = "txt_Username"
+        Me.txt_Username.Size = New System.Drawing.Size(279, 35)
+        Me.txt_Username.TabIndex = 4
+        Me.txt_Username.Text = "enter you username"
+        '
+        'txt_Password
+        '
+        Me.txt_Password.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_Password.ForeColor = System.Drawing.Color.Gray
+        Me.txt_Password.Location = New System.Drawing.Point(216, 218)
+        Me.txt_Password.Name = "txt_Password"
+        Me.txt_Password.Size = New System.Drawing.Size(279, 35)
+        Me.txt_Password.TabIndex = 5
+        Me.txt_Password.Text = "enter you password"
+        Me.txt_Password.UseSystemPasswordChar = True
+        '
+        'lbl_Login
+        '
+        Me.lbl_Login.AutoSize = True
+        Me.lbl_Login.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Login.Location = New System.Drawing.Point(79, 72)
+        Me.lbl_Login.Name = "lbl_Login"
+        Me.lbl_Login.Size = New System.Drawing.Size(142, 55)
+        Me.lbl_Login.TabIndex = 6
+        Me.lbl_Login.Text = "Login"
+        '
+        'lbl_WelcomeBack
+        '
+        Me.lbl_WelcomeBack.AutoSize = True
+        Me.lbl_WelcomeBack.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_WelcomeBack.ForeColor = System.Drawing.Color.OliveDrab
+        Me.lbl_WelcomeBack.Location = New System.Drawing.Point(111, 128)
+        Me.lbl_WelcomeBack.Name = "lbl_WelcomeBack"
+        Me.lbl_WelcomeBack.Size = New System.Drawing.Size(99, 16)
+        Me.lbl_WelcomeBack.TabIndex = 7
+        Me.lbl_WelcomeBack.Text = "Welcome Back"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(555, 553)
+        Me.Controls.Add(Me.lbl_WelcomeBack)
+        Me.Controls.Add(Me.lbl_Login)
+        Me.Controls.Add(Me.txt_Password)
+        Me.Controls.Add(Me.txt_Username)
+        Me.Controls.Add(Me.lbl_Password)
+        Me.Controls.Add(Me.lbl_Usernam)
+        Me.Controls.Add(Me.btb_Login)
+        Me.Controls.Add(Me.CheckBox_Showpassword)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox_Showpassword As CheckBox
+    Friend WithEvents btb_Login As Button
+    Friend WithEvents lbl_Usernam As Label
+    Friend WithEvents lbl_Password As Label
+    Friend WithEvents txt_Username As TextBox
+    Friend WithEvents txt_Password As TextBox
+    Friend WithEvents lbl_Login As Label
+    Friend WithEvents lbl_WelcomeBack As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    'dim correct username password
+    Private corectUsername As String = "ratchavit"
+    Private corectPassword As String = "10762"
+    Private Function login(username As String, password As String) As Boolean
+        If username = corectUsername AndAlso password = corectPassword Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub txt_Username_TextChanged(sender As Object, e As EventArgs) Handles txt_Username.GotFocus
+        txt_Username.Text = ""
+    End Sub
+
+    Private Sub txt_Password_Enter(sender As Object, e As EventArgs) Handles txt_Password.GotFocus
+        txt_Password.Text = ""
+    End Sub
+
+    Private Sub btb_Login_Click(sender As Object, e As EventArgs) Handles btb_Login.Click
+        Dim username As String = txt_Username.Text
+        Dim password As String = txt_Password.Text
+        If login(username, password) Then
+            MessageBox.Show("ล็อกอินสำเร็จ", "ผลลัพท์", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("ข้อมูลไม่ถูกต้อง", "ผลลัพท์", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub CheckBox_Showpassword_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox_Showpassword.CheckedChanged
+
+    End Sub
+
+End Class
